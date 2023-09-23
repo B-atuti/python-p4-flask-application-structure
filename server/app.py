3f3d75f869e7f1700a9dd1a52bdc0d6e46526f5d
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-23 17:32:02 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1> Welcome to my app!! </h1>"
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1> Welcome {username}</h1>'
+
+@app.route('/about')
+def about():
+    return f'<h1>This is the my about page.</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
+    
